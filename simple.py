ad029cde1b6252c0fe94e911220d215d4cf2f016
winh321 <69839416+winh321@users.noreply.github.com>
2024-08-17 02:08:32 +0630

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,9 @@
+import pandas as  pd
+from sklearn.tree import DecisionTreeClassifier
+music_data = pd.read_csv('music.csv')
+X = music_data.drop(columns=['genre'])
+y = music_data['genre']
+model = DecisionTreeClassifier()
+model.fit(X,y)
+predictions = model.predict([[35,1],[22,0]])
+predictions
